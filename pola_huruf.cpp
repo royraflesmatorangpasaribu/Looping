efe9366bcc74b8a45ca5acd891774ebaf9ee56ce
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-19 20:51:57 +0700

    Program C++_pola

---
+++ pola_huruf.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	
+	for(int i=0; i<n; i++){
+		for(int j=n-1; j>i; j--){
+			cout<<" ";
+		}
+		for(int k=0; k<=i; k++){
+			cout<<(char)(k+65);
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
