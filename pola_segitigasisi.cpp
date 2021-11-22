d909292b06e8f6ffa42704e6807ead45aaba9f4c
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-22 10:57:32 +0700

    Program C++_pola

---
+++ pola_segitigasisi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	int i, j, s;
+	
+	for(i=1; i<=n; i++){
+		for(s=n-1; s>=i; s--){
+		cout<<" ";
+		}
+		for(j=1; j<=i; j++){
+			if(i%2!=0)
+				cout<<(char)(i+64)<<" "; //get charakter
+			else
+				cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
