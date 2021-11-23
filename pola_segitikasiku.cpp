2bcdf4e11402179a63e5f68f236fc845032ea0bb
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-23 23:32:43 +0700

    Program C++_pola

---
+++ pola_segitikasiku.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+//di dalam c++ minimal ada satu fungsi()
+int main(){
+	//variabel yang digunakan
+	int i, j, k, tinggi= 5;
+	
+	//looping pola 
+	for(i=tinggi; i>=1; i--){
+		for(k=tinggi-1; k>=1; k--){
+			//output spasi
+			cout<<" ";
+		}
+		for(j=i; j>=1; j--){
+			cout<<j<<" ";
+		}
+		//output baris baru
+		cout<<endl;
+	}
+	//karena menggunakan fungsi main()
+	return 0;
+}
