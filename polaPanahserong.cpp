f53a994b661932d578b595dd6a73475e146eecbb
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-30 00:25:00 +0700

    update Bentuk output Program C++_pola

--- polaPanahserong.cpp
+++ polaPanahserong.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main(){
-	int n=5;
+	int n=7;
 	
 	for(int i=1; i<=n; i++)
 	{
 		for(int j=1; j<=n; j++)
 		{
 			if(i==1||j==n||j==n-i+1){
-				cout<<"*";
+				cout<<"* ";
 			}
 			else{
 				cout<<"  ";		
