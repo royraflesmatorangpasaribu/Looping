5747600ab3f9025fe9295a9f5d19afd5b21c661d
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-04 21:23:13 +0700

    Add files via upload

---
+++ polakurangdariAngka.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//fungsi utama dalam program
+int main(){
+	//deklarasi variabel
+	int n=7, x=n/2+1;
+	
+	//looping pola
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=x; j++){
+			if(j==x)
+			cout<<i;
+			else
+			cout<<"  "; // 2 spasi
+		}
+		cout<<endl;
+		if(i<=n/2)
+		x--;
+		else 
+		x++;
+	}
+	//karena menggunakan fungsi main()
+	return 0;
+}
