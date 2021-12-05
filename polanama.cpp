d1bc1f628d449406f3543bd87f2b133222ff117d
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-05 20:56:24 +0700

    Update polanama.cpp

--- polanama.cpp
+++ polanama.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 //minimal terdapat satu fungsi dalam bahasa C++
 int main(){
-	char str[]="ROY";
+	//variabel yang digunakan
+	char str[]="ROY"; //disini bebas mau menggunakan kalimat apa disini karena nama saya Roy jadi saya masukkan roy 
 	int len=strlen(str); //Fungsi strlen() digunakan untuk menghitung panjang string.
 	int i, j;
 	
-	//looping
+	//looping yang membentuk pola
 	for(i=0; i<len; i++){
 		for(j=0; j<len; j++){
 			if(i==len/2)
