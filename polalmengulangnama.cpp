31bb90ed44f644f8f931ee255bc113aa95448e6f
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-05 21:01:50 +0700

    Update polalmengulangnama.cpp

--- polalmengulangnama.cpp
+++ polalmengulangnama.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
 
 //minimal terdapat satu fungsi dalam bahasa C++
 int main(){
-	char arr[]="ROY";
+	char arr[]="ROY"; //bebas mau menggunakan nama siapa disini karena nama saya roy jadi saya pakai nama saya
 	int len=strlen(arr); //Fungsi strlen() digunakan untuk menghitung panjang string.
 	int i, j;
 	
-	//looping
+	//looping yang membentuk pola
 	for(i=0; i<len; i++){
 		for(j=0; j<=i; j++){
-			cout<<arr[j]<<" ";
+			cout<<arr[j]<<" "; 
 		}
 		cout<<endl;
 	}
