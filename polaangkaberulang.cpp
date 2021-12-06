e35cbb605ae7ff6583988b2d89aec518766a6ce3
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-06 19:40:20 +0700

    Add files via upload

---
+++ polaangkaberulang.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//minimal terdapat 1 fungsi
+int main(){
+	//variabel yang digunakan
+	int n=5, x=n;
+	
+	//looping 
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=x; j++){
+			if(i<=n/2+1){
+				cout<<(j+i-1)<<" "; //1 spasi
+			}
+			else{
+				cout<<(j+i-x)<<" "; //1 spasi
+			}
+		}
+		cout<<endl;
+		if(i<=n/2)
+		x-=2;
+		else
+		x+=2;
+	}
+	return 0;
+}
