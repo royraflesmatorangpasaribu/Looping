3cfc974c7f4461395e087ef505eaedf2fc6bcb53
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-08 20:32:53 +0700

    Add files via upload

---
+++ polaangka1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//minimal terdapat satu fungsi dalam program C++
+int main(){
+	int n=5;
+	int mid=n/2+1;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			if(j==mid || i==n || (i+j==mid && j<=mid))
+			cout<<"* ";
+			else
+			cout<<"  ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
