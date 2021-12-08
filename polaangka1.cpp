e281baa526037178c34a1f732cfe2f38db94cc23
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-08 20:34:04 +0700

    Update polaangka1.cpp

--- polaangka1.cpp
+++ polaangka1.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 //minimal terdapat satu fungsi dalam program C++
 int main(){
+	//deklarasi variabel 
 	int n=5;
 	int mid=n/2+1;
 	
+	//Looping pola * membentuk angka 1
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
 			if(j==mid || i==n || (i+j==mid && j<=mid))
