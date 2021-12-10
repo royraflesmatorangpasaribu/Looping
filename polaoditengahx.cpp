9a052afa8d3a999c7d4258f3478aa2f3786a6a13
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-10 20:43:38 +0700

    Update polaoditengahx.cpp

--- polaoditengahx.cpp
+++ polaoditengahx.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 //minimal terdapat 1 fungsi dalam program C++
 int main(){
+	//looping yang membentuk pola
 	for(int i=1; i<=5; i++){
 		for(int j=1; j<=5; j++){
 			if(i==3 && j==3)
@@ -10,7 +11,9 @@ int main(){
 			else
 			cout<<"x";
 		}
+		// 1 spasi
 		cout<<endl;
 	}
+	//karena menggunakan fungsi main()
 	return 0;
 }
