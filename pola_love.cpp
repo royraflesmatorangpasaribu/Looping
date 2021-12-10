5deda31cf3a32e12af15816a1be0761df63a8e83
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-10 21:03:56 +0700

    Update pola_love.cpp

--- pola_love.cpp
+++ pola_love.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(){
 	//delarasi variabel
 	int row, col;
+	
+	//looping yang membentuk pola
 	for(row=0; row<6; row++){
 		for(col=0;col<7;col++){
 			if((row==0 && col%3!=0) || (row==1 && col&& col%3==0) || (row-col==2) || (row+col==8)){
-				cout<<"* ";
+				cout<<"* ";  // 1 karakter lalu spasi
 			}
 			else{
 				cout<<"  ";
