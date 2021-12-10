1f0dd625c8820e2bab6132fb5048679035b470d3
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-10 21:01:55 +0700

    Add files via upload

---
+++ pola_love.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int row, col;
+	for(row=0; row<6; row++){
+		for(col=0;col<7;col++){
+			if((row==0 && col%3!=0) || (row==1 && col&& col%3==0) || (row-col==2) || (row+col==8)){
+				cout<<"* ";
+			}
+			else{
+				cout<<"  ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
