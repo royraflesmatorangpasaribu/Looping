e32e09e8873dc214e22d961a08de52f7576a9ea7
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-11 23:06:35 +0700

    Update hurufR.cpp

--- hurufR.cpp
+++ hurufR.cpp
@@ -7,8 +7,9 @@ int main(){
 	//deklarasi variabel
 	int n=5;
 	int i, j;
-	
 	int x=1;
+	
+	//looping yang membentuk huruf R
 	for(i=1; i<=n; i++){
 		for(j=1; j<=n/2+1; j++){
 			if(j==1 || i==1 || i==n/2+1 ||(i<=n/2 && j==n/2+1) || j==x){
