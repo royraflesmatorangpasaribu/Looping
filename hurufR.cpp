8261b01fb5c4fb9a841b2d0affa3f7a385a455ee
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-11 23:05:22 +0700

    Program C++

---
+++ hurufR.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	int i, j;
+	
+	int x=1;
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n/2+1; j++){
+			if(j==1 || i==1 || i==n/2+1 ||(i<=n/2 && j==n/2+1) || j==x){
+				cout<<"R ";
+			}
+			else{
+				cout<<"  ";
+			}
+		}
+		if(i>n/2)
+		x++;
+		cout<<endl;
+	}
+	return 0;
+}
