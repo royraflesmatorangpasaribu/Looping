c86d19645e91a739af57159432d5e4997f369d8f
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-12 21:20:50 +0700

    Update hurufY.cpp

--- hurufY.cpp
+++ hurufY.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main(){
 	//deklarasi variabel
 	int n=5;
-
+	
+	//looping yang membentuk
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
 			if((j==i||j==n-i+1)&& i<=n/2+1 ||(j==n/2+1 && i>n/2)){
-				cout<<"Y ";
+				cout<<"Y ";	// huruf Y lalu spasi
 			}
 			else{
-				cout<<"  ";
+				cout<<"  ";	// 2 spasi 
 			}
 		}
-		cout<<endl;
+		cout<<endl;	// baris membantuk baru
 	}
+	//karena menggunakan fungsi main()
 	return 0;
 }
