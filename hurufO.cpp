e1187c5ed7a074f1230e55249b2fa579e8db3f98
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-12 21:07:20 +0700

    Update hurufO.cpp

--- hurufO.cpp
+++ hurufO.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 using namespace std;
 
+//fungsi utama
 int main(){
+	
+	//deklarasi variabel yang digunakan
 	int n=5;
 	int i, j;
-	
 	int x=1;
+	
+	//looping yang membentuk pola
 	for(i=1; i<=n; i++){
 		for(j=1; j<=n; j++){
 			if(j==1 || j==n || i==1 ||i==n){
-				cout<<"O ";
+				cout<<"O "; // huruf 0 lalu 1 spasi
 			}
 			else{
 				cout<<"  ";
