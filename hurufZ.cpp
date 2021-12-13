c45aa22197e3db2d1b8f6337e914d9084cf8478b
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-13 18:49:32 +0700

    Update hurufZ.cpp

--- hurufZ.cpp
+++ hurufZ.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main(){
+	//deklarasi variabel
 	int n=5;
-
+	
+	//looping yang membentuk pola huruf Z
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
 			if(i==1 || i==n || j==n-i+1){
-				cout<<"Z ";
+				cout<<"Z "; // 1 karakter lalu 1 spasi
 			}
 			else{
-				cout<<"  ";
+				cout<<"  "; // 2 spasi
 			}
 		}
 		cout<<endl;
