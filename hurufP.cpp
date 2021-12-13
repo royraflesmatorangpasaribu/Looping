9fe570630481652bac9a987836a0460dd8363660
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-13 18:45:47 +0700

    Update hurufP.cpp

--- hurufP.cpp
+++ hurufP.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//minimal terdapat satu fungsi utama
 int main(){
+	
+	//deklarasi variabel
 	int n=5;
 	int x=(n/2)+1;
 	int i, j;
 	
+	//looping yang membentuk pola Z
 	for(i=1; i<=n; i++){
 		for(j=1; j<=n; j++){
 			if((i==1 && j<=x )|| j==1 ||(j==x && i<=x) ||(i==x && j<=x)){
-				cout<<"* ";
+				cout<<"* "; // 1 karakter lalu spasi
 			}
 			else{
 				cout<<"  ";
