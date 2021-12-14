1a6a7f85daf804a4121f7fb6c9cbc995819d87a1
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-14 20:09:21 +0700

    Update polaXO.cpp

--- polaXO.cpp
+++ polaXO.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 
+//minimal terdapat satu fungsi
 int main(){
+	//looping yang membentuk pola
 	for(int i=1; i<=5; i++){
 		for(int j=1; j<=5; j++){
 			if(i<=j)
