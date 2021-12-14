e050d47c9eb04f0f6352f81bbebcafa2e749a030
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-14 20:15:08 +0700

    Update kotakpagar.cpp

--- kotakpagar.cpp
+++ kotakpagar.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 //minimal terdapat satu fungsi
 int main(){
+	//deklarasi variabel
 	int n=5;
+	//looping yang membentuk pola
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
 			if((j==1||j==n)||(i==1||i==n))
