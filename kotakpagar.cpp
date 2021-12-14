2ead68f5964d08ed931ae7ea77bf404653931c9a
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-14 20:15:35 +0700

    Update kotakpagar.cpp

--- kotakpagar.cpp
+++ kotakpagar.cpp
@@ -9,7 +9,7 @@ int main(){
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
 			if((j==1||j==n)||(i==1||i==n))
-			cout<<"*";
+			cout<<"*";	//karakter yang akan ditampilkan di tepi
 			else
 			cout<<"#";
 		}
