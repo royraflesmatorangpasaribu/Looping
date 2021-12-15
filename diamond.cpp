5511d14a94ef66e2c5ed3be7b925d5069907e4f1
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-15 19:43:36 +0700

    Update diamond.cpp

--- diamond.cpp
+++ diamond.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//minimal terdapat satu fungsi
 int main(){
+	
+	//deklarasi variabel
 	int n=9;
 	int px=n/2+1;
 	int i,j;
 	
+	//looping yang membentuk pola
 	for(i=1; i<=n; i++){
 		for(j=1; j<=n; j++){
 			if(j==px || j==n-px+1){
 				cout<<"*";
 			}
 			else{
-				cout<<" ";
+				cout<<" ";	// 1 spasi
 			}
 		}
 		if(i<=n/2)
