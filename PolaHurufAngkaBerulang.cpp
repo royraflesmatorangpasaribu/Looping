67ada75171dba3fa94f96bce650b494eadcc24e0
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-16 22:35:52 +0700

    Update PolaHurufAngkaBerulang.cpp

--- PolaHurufAngkaBerulang.cpp
+++ PolaHurufAngkaBerulang.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 //minimal terdapat satu fungsi dalam program c++
 int main(){
-	
 	//deklarasi variabel
 	int n=5;
-	
 	//looping yang membentuk pola
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=n; j++){
