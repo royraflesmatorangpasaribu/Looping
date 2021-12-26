9ec714ffd0dab79cf31a6147608366cf5f1b325a
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-26 16:46:39 +0700

    Add files via upload

---
+++ berpangkat3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//inisilisasi variabel n
+	//berapa banyak yang ingin ditampilkan
+	int n=7;
+	
+	for(int i=1; i<=n; i++){
+		cout<<" "<<(i*i*i);
+	}
+}
