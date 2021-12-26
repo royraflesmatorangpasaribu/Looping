2c380e8dd227f5ab9a1672b1249285521b139b94
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-26 20:53:57 +0700

    Add files via upload

---
+++ bilanganGeometri.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a, r, n;
+	
+	cout<<"Program Bilangan Geometri"<<endl;
+	cout<<"by Royrafmp\n\n"<<endl;
+	cout<<"Masukkan Bilangan Pertama              : ";
+	cin>>a;
+	cout<<"Masukkan Rasio Bilangan                : ";
+	cin>>r;
+	cout<<"Masukkan Batas yang ingin di tampilkan : ";
+	cin>>n;
+	
+	cout<<"\n\nBarisan Bilangan Geometrinya adalah : \n";
+	for(int i=1; i<=n; i++){
+		cout<<" "<<a;
+		a=a*r;
+	}
+	
+}
