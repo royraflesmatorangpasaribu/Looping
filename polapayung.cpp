d04f6a093dc608bb83257b13aefc22483254ca62
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-27 07:53:06 +0700

    Add files via upload

---
+++ polapayung.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=7;
+	int x=n/2+1;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			if(j==n/2+1||(j<=x&& j>=n-x+1&& i<=n/2+1)){
+				cout<<"* "; //1 spasi
+			}
+			else{
+				cout<<"  "; //2 spasi
+			}
+		}
+		cout<<endl;
+		x++;
+	}
+	return 0;
+}
