24d91ce09df0f0b7dd45fa80d76c11ea4a65076b
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-03 09:48:28 +0700

    Update angkasegitigasiku.cpp

--- angkasegitigasiku.cpp
+++ angkasegitigasiku.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(){
 	//deklarasi variabel
 	int n=5;
+	
+	//looping yang membentuk pola
 	for(int i=1; i<=n; i++){
 		for(int j=1; j<=i; j++){
 			if(j==1|| j==i|| i==n){
