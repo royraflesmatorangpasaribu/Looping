949a04fdf2d387a6c10574a8c2e5558db75be63e
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-06 07:46:31 +0700

    Add files via upload

---
+++ segitigasiku.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+//program ini dibuat oleh roy rafles matorang pasaribu
+//pada 05 Januari 2022
+
+//minimal terdapat satu fungsi
+int main(){
+	//deklarasi variabel
+	int n=5;
+	
+	//looping yang membentuk pola
+	for(int i=0; i<=n; i++){
+		for(int j=0; j<=i; j++){
+		if(i>j)
+		cout<<"*";
+		else
+		cout<<i;	
+		}
+		cout<<endl;
+	}
+	//karena menggunaka fungsi main()
+	return 0;
+}
