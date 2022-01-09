5da7d0883a6c7d013598d594ef76d0f323909f51
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-09 18:58:12 +0700

    Add files via upload

---
+++ polaplusx.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i, j;
+	
+	for(i=1; i<=5; i++){
+		for(j=1; j<=5; j++){
+			if(j==3||i==3)
+			cout<<"x";
+			else
+			cout<<"o";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
