ca371d67f8e37cd50d4fecca6d13cbf78a6edf27
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-11 06:29:44 +0700

    Add files via upload

---
+++ polasegitigabawahHuruf.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=5;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<2*n; j++){
+			if(j==i || j==2*n-i){
+				cout<<(char)(j+64);
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
