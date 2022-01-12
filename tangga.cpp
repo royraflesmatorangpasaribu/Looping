8ef08e3d325674f493d3ac8d5fc9274a486f18e8
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-12 08:31:47 +0700

    Update tangga.cpp

--- tangga.cpp
+++ tangga.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
+
+//program dibuat oleh roy rafles matorang pasaribu
+//pada 12 Januari 2022
+
+//minimal terdapat satu fungsi
 int main(){
+	//mendeklrasasikan variabel
 	int i, j, k, l;
 	for(i=1; i<=3; i++){
 		for(j=2; j>0; j--){
