5fb17d173ae6f26a9994627d5309c4e5af0e2dd5
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-19 16:56:27 +0700

    Update EthereumConvertUSD.cpp

--- EthereumConvertUSD.cpp
+++ EthereumConvertUSD.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //dibuat oleh Roy Rafles Matorang Pasaribu 
 //Pada 19 Januari 2022
 
+//fungsi utama
 int main(){
 	cout<<"Program Menghitung Rata-Rata dan Total serta Convert ke USD"<<endl;
 	cout<<"Masukkan Jumlah Ethereum Anda : ";
