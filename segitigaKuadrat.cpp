3a9851b36fcebed384750e4a2659d6b2b09412cc
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-02-11 20:33:53 +0700

    Add files via upload

---
+++ segitigaKuadrat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i, j;
+	int n=4, x=1;
+	
+	for(i=1; i<=n; i++){
+		for(j=1; j<=i; j++){
+			cout<<(x*x)<<"  ";
+			x++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
