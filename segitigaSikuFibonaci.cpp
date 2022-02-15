800bbeadb9414c1935a2fbca6f53afaf63745298
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-02-15 22:17:48 +0700

    Add files via upload

---
+++ segitigaSikuFibonaci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i, j;
+	int n1=0, n2=1, r=n1+n2;
+	
+	for(int i=1; i<=5; i++){
+		for(int j=1; j<=i; j++){
+			cout<<r<<" ";
+			r=n1+n2;
+			n1=n2;
+			n2=r;
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
